9dd65b27e3d09c2ffe7b8654f03cf178c6ca8b11
gargargargar <gargar070402@gmail.com>
2018-11-12 12:29:42 -0500

    layout.pug update

--- week-7/express-locallibrary-tutorial/routes/index.js
+++ week-7/express-locallibrary-tutorial/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // GET home page.
 router.get('/', function(req, res) {
-  res.render('index', {title: "Test title!"});
+  res.render('index', {title: "Garcia's Test Local Library Site"});
 });
 
 module.exports = router;
