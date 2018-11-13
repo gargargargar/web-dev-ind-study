31f12bfc21ba6ba75d3160db24dbc6a37e7b5131
gargargargar <gargar070402@gmail.com>
2018-11-13 09:27:48 -0500

    Update to mdn tutorial part 5.7

--- week-7/express-locallibrary-tutorial/routes/index.js
+++ week-7/express-locallibrary-tutorial/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // GET home page.
 router.get('/', function(req, res) {
-  res.render('index', {title: "Garcia's Test Local Library Site"});
+  res.redirect('/catalog');
 });
 
 module.exports = router;
